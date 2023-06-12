a7e37fdaa9399afd758d2a92101dc54968764f83
0xC00005 <0xc00005@gmail.com>
2023-06-12 11:59:19 -0400

    test

--- spx.py
+++ spx.py
@@ -322,6 +322,7 @@ def sum_penalty(X):
 	return sumPenalty
 
 
+
 def optimize(X_curr):
 	# Start with pivotmds or neato stress majorization or cmdscale as in the paper
 	# Or use X with a random initialization
